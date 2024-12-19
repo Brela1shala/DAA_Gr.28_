50b382a8aac7b942388aa36edec59934acd0632f
BleronRrmoku <bleron.rrmoku@student.uni-pr.edu>
2024-12-19 18:07:38 +0100

    added to main

--- supply_chain.py
+++ supply_chain.py
@@ -14,3 +14,14 @@ class SupplyChainGUI(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         main_layout = QVBoxLayout(central_widget)
+
+         # Button to generate tables
+        generate_button = QPushButton("Generate Tables")
+        generate_button.clicked.connect(self.generate_tables)
+        main_layout.addWidget(generate_button)
+
+        # Optimize button
+        optimize_button = QPushButton("Optimize")
+        optimize_button.clicked.connect(self.optimize)
+        main_layout.addWidget(optimize_button)
+         
